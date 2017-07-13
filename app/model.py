fcd7a0a56970e49b33cc43b48db2322ac4ffd785
chadwalt <chadwalt@outlook.com>
2017-07-13 04:58:09 +0300

    Adding test for edit bucketlist item and adding the edit_item  method to the bucket class

--- app/model.py
+++ app/model.py
@@ -19,22 +19,40 @@ class Users :
 
 
 class Bucket:
-    bucketlist = []
+    bucketlist_items = []
 
     ##Add buckets.
-    def add_item():
-        pass
+    def add_item(self, name, time):
+        ## Check if the name and time have been provided.
+        if name and time:
+            ## Create a dictionary to hold the new item.
+            id = 1
+            if len(self.bucketlist_items):
+                id = len(self.bucketlist_items)
+
+            dict = {'id': id, 'name': name, 'time': time}
+            self.bucketlist_items.append(dict)
+            return True
+
+        return False
 
     ## Edit bucket
-    def edit_item():
-        pass
+    def edit_item(self, id, name):
+        ## The id cannnot be empty
+        if id :
+            for item in self.bucketlist_items:
+                if item['id'] == id:
+                    item['name'] = name
+                    return True
+                    
+        return False
 
     ## Delete bucket
-    def delete_item():
+    def delete_item(self):
         pass
 
     ## List buckets.
-    def list_items():
+    def list_items(self):
         pass             
 
 class BucketItem:        
--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -1,18 +1,22 @@
 import module_import
-from app.model import Users
+from app.model import Bucket
 import unittest
 
 class BaseTest(unittest.TestCase):
     ## Login set up now.
     def setUp(self):
-        self.users = Users()
+        self.bucket = Bucket()
 
     ## Test empty fields.
-    def test_adding_bucket(self):
-        ##app_login = App()
-        result = self.users.add_bucket('', '')
+    def test_adding_bucket_item(self):
+        result = self.bucket.add_item('', '')
         ##self.assertEqual(True, result, 'Username and password cannot be empty')
-        self.assertTrue(result, "Bucket Name cannot be empty")
+        self.assertFalse(result, "Bucket Name cannot be empty")
+
+    ## Test empty fields.
+    def test_edit_bucket_item(self):
+        result = self.bucket.edit_item('', '')
+        self.assertFalse(result, "ID and name cannot be empty")        
 
     
 if __name__ == '__main__':
